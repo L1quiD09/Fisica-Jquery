99a1c2b782bc3937e70448a4861f9f98977d4c10
Andres Rodriguez <androd9002@gmail.com>
2022-07-11 10:34:08 -0500

    main

---
+++ fisica.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    
+    let resultado = 0;
+    let cont = 0;
+
+
+    $("#boton1").click(function (e) {
+        cont++;
+        let distancia = $("#first_name1").val();
+        let tiempo = $("#first_name2").val();
+        e.preventDefault();
+        resultado = distancia/tiempo;
+        resultado = resultado.toFixed(5) 
+         $("#first_name3").val(resultado + " m/s");
+         $("#tabla").append('<tr><td text-black> n: '+cont+ '</td><td>' + resultado + ' m/s </td><td>' + distancia+ ' m </td><td>' +tiempo+ ' s </td></tr>');
+    });
+    $("#boton2").click(function (e) { 
+        e.preventDefault();
+        $("#tabla tbody > tr").empty();
+
+    });
+});
